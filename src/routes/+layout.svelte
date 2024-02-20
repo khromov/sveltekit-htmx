2dd1748cfb53142ac69a4c1abbc4c242268b4e44
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2024-02-20 22:59:16 +0100

    Add basic layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+<h1>
+   {$page?.data.title || 'No title found in page store'} 
+</h1>
+
+<div class="menu">
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+</div>
+
+<div class="content">
+    <slot />
+</div>
+
+<style>
+    .content {
+        margin-top: 8px;
+    }
+</style>
