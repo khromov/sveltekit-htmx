d8486b45636ec7035765d5b111213a0bf59642c5
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2025-04-28 00:49:59 +0200

    Update hooks.server.ts

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,15 +4,17 @@ import { version } from '$app/environment';
 
 export const htmx: Handle = ({ event, resolve }) => {
 	const isHtmxRequest = event.request.headers.get('hx-request') === 'true';
-	console.log('isHtmxRequest', isHtmxRequest);
+	const hxTarget = event.request.headers.get('hx-target');
+	// TODO: casing of above, and if hx-target, return partial?
 	return resolve(event, {
 		transformPageChunk: ({ html }) =>
 			html.replace(
 				'%htmx%',
-				!isHtmxRequest ? 
-                `<script src="/htmx.min.js?version=${version}"></script>
+				!isHtmxRequest
+					? `<script src="/htmx.min.js?version=${version}"></script>
                 <script src="/htmx-loading-states.js?version=${version}"></script>
-                ` : ''
+                `
+					: ''
 			)
 	});
 };
